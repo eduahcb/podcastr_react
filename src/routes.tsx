8180c08f8415f62f804b85b4662da4500ed05270
Eduardo Henrique Costa <eduardohenriquecosta@anorak.local>
2025-03-03 13:37:04 -0300

    feat: importing AuthLayout component

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
 
+import {  AuthLayout } from '@/pages/auth/layout'
+
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route element={<AuthLayout />}>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
